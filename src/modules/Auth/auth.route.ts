852fdbc436178239d539e9588a0c30a3322d644b
NafizUddin <nafizuddin.ctg@gmail.com>
2024-12-14 23:49:05 +0600

    added change password route

--- src/modules/Auth/auth.route.ts
+++ src/modules/Auth/auth.route.ts
@@ -4,6 +4,8 @@ import validateRequest, {
 } from '../../middlewares/validateRequest';
 import { AuthControllers } from './auth.controller';
 import { AuthValidation } from './auth.validation';
+import auth from '../../middlewares/auth';
+import { UserRole } from '@prisma/client';
 
 const router = express.Router();
 
@@ -31,6 +33,17 @@ router.post(
   '/refresh-token',
   validateRequestCookies(AuthValidation.refreshTokenValidationSchema),
   AuthControllers.refreshToken,
+);
+
+router.post(
+  '/change-password',
+  auth(
+    UserRole.SUPER_ADMIN,
+    UserRole.ADMIN,
+    UserRole.CUSTOMER,
+    UserRole.VENDOR,
+  ),
+  AuthControllers.changePassword,
 );
 
 export const AuthRoutes = router;
